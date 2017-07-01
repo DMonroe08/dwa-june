2495f676b29740fc12181c2a8663fdb039df9831
Danielle Monroe <dmonroe08@fullsail.edu>
2017-06-30 21:55:04 -0400

    Adds meal.js code and initial test.js

---
+++ src/routes/meal.js
@@ -0,0 +1,39 @@
+
+
+module.exports = (getMeal) => {
+if(getMeal){
+  console.log(meal);
+}
+
+  return getMeal;
+};
+
+module.exports = (getMeals) => {
+foreach(meals){
+  console.log(meals);
+}
+
+  return getMeals;
+};
+
+module.exports = (createMeal) => {
+foreach(newMeal){
+  meals.push();
+}
+
+  return createMeal;
+};
+
+module.exports = (updateMeal) => {
+
+
+  return updateMeal;
+};
+
+module.exports = (removeMeal) => {
+if(removeMeal){
+  delete.meal;
+}
+
+  return removeMeal;
+};
