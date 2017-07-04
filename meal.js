b93c8d9efe632901caf2e207cf0f8e36ba303c7c
Danielle Monroe <dmonroe08@fullsail.edu>
2017-07-03 20:54:39 -0400

    meal.js test.js

--- meal.js
+++ meal.js
@@ -1,39 +1,61 @@
 
+const mealArr = [];
 
-module.exports = (getMeal) => {
-if(getMeal){
-  console.log(meal);
-}
+const mealItem = [
+{mealID: 'kj3ns9djd', name: 'Pizza'},
+{mealID: '3kn39sk3n', name: 'Burger and Fries'},
+{mealID: '3jwkx9ejk', name: 'Spaghetti'},
+{mealID: '9dm33md08', name: 'Stir Fry'},
+{mealID: 'k3k39d9si', name: 'Cereal'},
+{mealID: '4k49djn0x', name: 'Peanut Butter and Jelly'},
+{mealID: 'u7yg665f8', name: 'Wings'},
+{mealID: '2ksiud837', name: 'Bagel'},
+{mealID: '3kis93klo', name: 'Hot Dog'}
+
+];
+
+exports.getMeal = (mealArr[index]) => {
+
+})
 
   return getMeal;
 };
 
-module.exports = (getMeals) => {
-foreach(meals){
-  console.log(meals);
-}
+exports.getMeals = function(mealArr){
+//   getMeal.findAll(req.body, (data) => {
+//     res.status(200).json(data);
+//   }, (err) => {
+//     res.status(500).json(err);
+//   })
+// }
+
+for(var index = 0; index < mealArr.length; index++){
+  var name = mealArr[name];
+  console.log('Your meal is ' + mealArr.name);
 
   return getMeals;
 };
 
-module.exports = (createMeal) => {
+exports.createMeal = (mealArr, newMeal) => {
 foreach(newMeal){
-  meals.push();
+  mealArr.push();
 }
 
   return createMeal;
 };
 
-module.exports = (updateMeal) => {
-
+exports.updateMeal = () => {
+mealArr.push();
 
   return updateMeal;
 };
 
-module.exports = (removeMeal) => {
-if(removeMeal){
-  delete.meal;
-}
+exports.removeMeal = (mealRemoved) => {
+// if(removeMeal){
+//   delete.meal;
+// }
+
+const removeM = delete.mealArr.mealID;
 
   return removeMeal;
 };
--- test.js
+++ test.js
@@ -1,1 +1,44 @@
-const meal = (./meal.js);
+const meal = ('./meal.js');
+
+const mealItems = [
+{mealID: 'kj3ns9djd', name: 'Pizza'},
+{mealID: '3kn39sk3n', name: 'Burger and Fries'},
+{mealID: '3jwkx9ejk', name: 'Spaghetti'},
+{mealID: '9dm33md08', name: 'Stir Fry'},
+{mealID: 'k3k39d9si', name: 'Cereal'},
+{mealID: '4k49djn0x', name: 'Peanut Butter and Jelly'},
+{mealID: 'u7yg665f8', name: 'Wings'},
+{mealID: '2ksiud837', name: 'Bagel'},
+{mealID: '3kis93klo', name: 'Hot Dog'},
+{mealID: 'o339ddje9', name: 'Roast Pork'}
+];
+// foreach(function(mealItems){
+//   var meal = mealItems[index];
+//   console.log('Your meal is ' + meal);
+// })
+
+
+if(meal.getMeals = true){
+// for(var index = 0; index < mealItems.length; index++){
+//   var name = mealItems[name];
+//   console.log('Your meal is ' + name);
+// }
+var getMeals = meal.getMeals;
+console.log(getMeals);
+// console.log('meals listed');
+}
+
+if(meal.createMeal == true){
+
+}
+
+if(meal.updateMeals = true){
+//meal.updateMeal({mealID: 12, name: 'sandwich'});
+console.log(mealItems);
+}
+
+if(meal.removeMeal == true){
+var removed = meal.removeMeal[4];
+var newList = mealItems - removed;
+  console.log(newList);
+}
